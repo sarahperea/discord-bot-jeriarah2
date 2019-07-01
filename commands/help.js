116dbba89db90b2d26053f041d386e2efd413524
Sarah D. Perea <sdperea.1@gmail.com>
2019-07-01 20:16:08 +0800

    Remove help.js

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async(client, message, args) => {
-	message.channel.send(embed);							
-}
-
-module.exports.help = {
-	name: 'help'
-}
