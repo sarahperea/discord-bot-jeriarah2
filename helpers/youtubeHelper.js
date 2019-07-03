61bf78e3ba85d58ba9577a768258e3b15abda91f
Sarah D. Perea <sdperea.1@gmail.com>
2019-07-03 09:46:01 +0800

    Add 'Searching songs ...' prompt

--- helpers/youtubeHelper.js
+++ helpers/youtubeHelper.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const apiKey = process.env.YOUTUBE_DATA_API_KEY_JERIARAH2;
 
 module.exports.searchByQueries = (queries, callback, connection, message) => {
+	message.channel.send(`**Searching songs ... :mag_right:**`);
 	axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${queries}&maxResults=5&key=${apiKey}`, {
     }).then(res => {
     	if (res.data) {
