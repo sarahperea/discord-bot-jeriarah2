6d9af9ea4c00b073f87efc24c8aa9cb4c79ceb24
Sarah D. Perea <sdperea.1@gmail.com>
2019-07-03 10:38:47 +0800

    Global variables for message and connection

--- commands/play.js
+++ commands/play.js
@@ -1,41 +1,43 @@
 const ytdl = require('ytdl-core');
 let youtubeHelper = require('../helpers/youtubeHelper.js')
 
-function playURL(url, connection, message) {
+function playURL(url) {
   const streamOptions = { seek: 0, volume: 5 };
   const stream = ytdl(url, { filter : 'audioonly' });
-  const dispatcher = connection.playStream(stream, streamOptions);
-  message.channel.send(`**Playing ${url}**`);
+  const dispatcher = connection_copy.playStream(stream, streamOptions);
+  message_copy.channel.send(`**Playing ${url}**`);
 }
 
-function getURL (res, connection, message) {
+function getURL (res) {
   if (res.items && res.items[0].id && res.items[0].id.videoId) {
     let videoId = res.items[0].id.videoId;
 
-    playURL(`https://www.youtube.com/watch?v=${videoId}`, connection, message)
+    playURL(`https://www.youtube.com/watch?v=${videoId}`, connection_copy, message_copy)
   }
 }
 
 module.exports.run = async(client, message, args) => {
+  message_copy = message
   let url_regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
   let channel = message.member.voiceChannel;
 
   if (args.length === 0) {
     message.reply("Sorry, this command requires a youtube video url to play. :P")
   } else {
-	if (channel) {
-      	channel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          if (args.length === 1 && args[0].match(url_regex)) {
-            playURL(args[0], connection)
-          } else {
-            let queries = args.join(',')
-            youtubeHelper.searchByQueries(queries, getURL, connection, message);
-          }
-        })
-        .catch((err)=>{
-          console.log(err)
-        });
+	  if (channel) {
+    	channel.join()
+      .then(connection => { // Connection is an instance of VoiceConnection
+        connection_copy = connection
+        if (args.length === 1 && args[0].match(url_regex)) {
+          playURL(args[0], connection)
+        } else {
+          let queries = args.join(',')
+          youtubeHelper.searchByQueries(queries, getURL, connection, message);
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      });
     } else {
       message.reply('You need to join a voice channel first!');
     }
